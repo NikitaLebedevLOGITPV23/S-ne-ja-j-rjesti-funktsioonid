df85e061c7e46695987bccf9672965bd1dd42b35
opilane <opilane@HP-CZC2349HT2>
2024-02-06 10:52:26 +0200

    Add project files.

---
+++ Sõne ja järjesti funktsioonid/Sõne_ja_järjesti_funktsioonid.py
@@ -0,0 +1,40 @@
+﻿   #1 Литералы строк
+
+S="str"
+index = 0
+while True:
+    print(S[index])
+    index += 1
+    if index == len(S):
+        break
+   
+   #2 Экранированные последовательности
+
+S="s\np\ta\nbbb"
+count = 0
+while True:
+    if count >= len(S):
+        break
+    count += 1
+print("Sümbolite arv reas :", count)
+   
+   #3 Повторение строки
+S1="Rida kordamine"
+count = 0
+while True:
+    if count < 3:
+        print(S1 * 3)
+        count += 1
+    else:
+        break
+
+   #4 Длина строки
+S="Joone pikkus"
+pikkus=0
+while True:
+    if pikkus<len(S):
+        pikkus+=1
+    else:
+        break
+print("Joone pikkus:", pikkus)
+
